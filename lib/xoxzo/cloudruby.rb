287d3aec01f2c8d5b37ba83e498f2b8f168621f9
Akira Nonaka <nonaka@mac.com>
2016-05-18 16:18:56 +0900

    add gem files.

---
+++ lib/xoxzo/cloudruby.rb
@@ -0,0 +1,7 @@
+require "xoxzo/cloudruby/version"
+
+module Xoxzo
+  module Cloudruby
+    # Your code goes here...
+  end
+end
---
+++ lib/xoxzo/cloudruby/version.rb
@@ -0,0 +1,5 @@
+module Xoxzo
+  module Cloudruby
+    VERSION = "0.1.0"
+  end
+end
