829ba036596994f9c92b52c93b547f05cc231451
Akira Nonaka <nonaka@mac.com>
2016-05-19 17:09:24 +0900

    WIP, send_sms implemented.

--- lib/xoxzo/cloudruby.rb
+++ lib/xoxzo/cloudruby.rb
@@ -1,7 +1,44 @@
 require "xoxzo/cloudruby/version"
 
+require "httparty"
+require 'json'
+
 module Xoxzo
   module Cloudruby
-    # Your code goes here...
+
+    class XoxzoRespose
+      def initalize
+        @status = nil
+        @message = Hash.new
+        @messages = []
+      end
+      attr_accessor :status,:message,:messages
+    end
+
+    class XoxzoClient
+      def initialize(sid,token)
+          @auth = {:username => sid, :password => token}
+          api_host = "https://api.xoxzo.com"
+          @xoxzo_api_sms_url = api_host + "/sms/messages/"
+          @xoxzo_api_voice_simple_url = api_host + "/voice/simple/playback/"
+      end
+
+      def send_sms(message, recipient, sender)
+        body = {"message" => message , "recipient" => recipient , "sender" => sender}
+        res = HTTParty.post(@xoxzo_api_sms_url, :basic_auth => @auth,
+                            :body => body.to_json,
+                            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+        if res.code == 201
+          xr = XoxzoRespose.new
+          xr.status = nil
+          xr.messages = JSON.parse(res.body)
+        else
+          xr = XoxzoRespose.new
+          xr.status = res.code
+          xr.message = JSON.parse(res.body)
+        end
+        return xr
+      end
+    end
   end
 end
--- spec/xoxzo/cloudruby_spec.rb
+++ spec/xoxzo/cloudruby_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
+include Xoxzo::Cloudruby
+
 describe Xoxzo::Cloudruby do
+  before(:each) do
+    sid = ENV['XOXZO_API_SID']
+    token = ENV['XOXZO_API_AUTH_TOKEN']
+    @xc = XoxzoClient.new(sid,token)
+    end
+
   it 'has a version number' do
     expect(Xoxzo::Cloudruby::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  xit 'test send sms success' do
+    res = @xc.send_sms(message="こんにちはRuby Lib です", recipient = "+818054695209", sender = "8108054695209")
+    expect(res.status).to eq(nil)
+    expect(res.message).to be nil
+    expect(res.messages[0].key?('msgid')).to be true
+  end
+
+  it 'test send sms faile, bad recipient format' do
+    res = @xc.send_sms(message="こんにちはRuby Lib です", recipient = "+8108054695209", sender = "8108054695209")
+    expect(res.status).to eq(400)
+    expect(res.message.key?('recipient')).to be true
+    expect(res.messages).to be nil
   end
 end
