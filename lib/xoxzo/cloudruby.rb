7b3a60d8d985c6e6b51b3d80c91261ff468e9a0b
Akira Nonaka <nonaka@mac.com>
2016-05-20 16:23:07 +0900

    WIP, get_simple_playback_status implemented. use json_safe_parse

--- lib/xoxzo/cloudruby.rb
+++ lib/xoxzo/cloudruby.rb
@@ -23,15 +23,15 @@ module Xoxzo
           @xoxzo_api_voice_simple_url = api_host + "/voice/simple/playback/"
       end
 
-      def send_sms(message, recipient, sender)
+      def send_sms(message:, recipient:, sender:)
         body = {"message" => message , "recipient" => recipient , "sender" => sender}
         res = HTTParty.post(@xoxzo_api_sms_url, :basic_auth => @auth,
                             :body => body.to_json,
                             :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
         if res.code == 201
-          xr = XoxzoRespose.new(messages: JSON.parse(res.body))
+          xr = XoxzoRespose.new(messages: json_safe_parse(res.body))
         else
-          xr = XoxzoRespose.new(errors: res.code,message: JSON.parse(res.body))
+          xr = XoxzoRespose.new(errors: res.code,message: json_safe_parse(res.body))
         end
         return xr
       end
@@ -40,9 +40,9 @@ module Xoxzo
         url = @xoxzo_api_sms_url + msgid
         res = HTTParty.get(url, :basic_auth => @auth)
         if res.code == 200
-          xr = XoxzoRespose.new(message: JSON.parse(res.body))
+          xr = XoxzoRespose.new(message: json_safe_parse(res.body))
         else
-          xr = XoxzoRespose.new(errors: res.code,message: JSON.parse(res.body))
+          xr = XoxzoRespose.new(errors: res.code,message: json_safe_parse(res.body))
         end
         return xr
       end
@@ -55,11 +55,41 @@ module Xoxzo
         end
         res = HTTParty.get(url, :basic_auth => @auth)
         if res.code == 200
-          xr = XoxzoRespose.new(messages: JSON.parse(res.body))
+          xr = XoxzoRespose.new(messages: json_safe_parse(res.body))
         else
-          xr = XoxzoRespose.new(errors: res.code, message: JSON.parse(res.body))
+          xr = XoxzoRespose.new(errors: res.code, message: json_safe_parse(res.body))
         end
         return xr
+      end
+
+      def call_simple_playback(caller:, recipient:, recording_url:)
+        body = {"caller" => caller , "recipient" => recipient , "recording_url" => recording_url}
+        res = HTTParty.post(@xoxzo_api_voice_simple_url, :basic_auth => @auth,
+                            :body => body.to_json,
+                            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+        if res.code == 201
+          xr = XoxzoRespose.new(messages: json_safe_parse(res.body))
+        else
+          xr = XoxzoRespose.new(errors: res.code,message: json_safe_parse(res.body))
+        end
+        return xr
+      end
+
+      def get_simple_playback_status(callid:)
+        url = @xoxzo_api_voice_simple_url + callid
+        res = HTTParty.get(url, :basic_auth => @auth)
+        if res.code == 200
+          xr = XoxzoRespose.new(message: json_safe_parse(res.body))
+        else
+          xr = XoxzoRespose.new(errors: res.code,message: json_safe_parse(res.body))
+        end
+        return xr
+      end
+
+      def json_safe_parse(s)
+        return JSON.parse(s)
+      rescue JSON::ParserError => e
+        return {}
       end
     end
   end
--- spec/xoxzo/cloudruby_spec.rb
+++ spec/xoxzo/cloudruby_spec.rb
@@ -7,6 +7,8 @@ describe Xoxzo::Cloudruby do
   before(:each) do
     sid = ENV['XOXZO_API_SID']
     token = ENV['XOXZO_API_AUTH_TOKEN']
+    @test_recipient = ENV['XOXZO_API_TEST_RECIPIENT']
+    @test_mp3_url = ENV['XOXZO_API_TEST_MP3']
     @xc = XoxzoClient.new(sid,token)
     end
 
@@ -15,7 +17,7 @@ describe Xoxzo::Cloudruby do
   end
 
   xit 'test send sms success and get sent status' do
-    res = @xc.send_sms(message="こんにちはRuby Lib です", recipient = "+818054695209", sender = "8108054695209")
+    res = @xc.send_sms(message: "こんにちはRuby Lib です", recipient: @test_recipient, sender: "8108012345678")
     expect(res.errors).to be nil
     expect(res.message).to eq({})
     expect(res.messages[0].key?('msgid')).to be true
@@ -31,7 +33,7 @@ describe Xoxzo::Cloudruby do
   end
 
   it 'test send sms faile, bad recipient format' do
-    res = @xc.send_sms(message="こんにちはRuby Lib です", recipient = "+8108054695209", sender = "8108054695209")
+    res = @xc.send_sms(message: "こんにちはRuby Lib です", recipient: "+0808012345678", sender: "8108012345678")
     expect(res.errors).to eq 400
     expect(res.message.key?('recipient')).to be true
     expect(res.messages).to eq []
@@ -63,10 +65,24 @@ describe Xoxzo::Cloudruby do
     end
   end
 
-  it 'test get sent sms list fail, bad date' do
-    res = @xc.get_sent_sms_list(sent_date: "=2016-15-45")
-    expect(res.errors).to be 400
-    expect(res.message.key?('sent_date')).to be true
+  xit 'test simple voice playback success' do
+    res = @xc.call_simple_playback(caller:"810812345678",recipient: @test_recipient,recording_url: @test_mp3_url)
+    expect(res.errors).to be nil
+    expect(res.message).to eq({})
+    expect(res.messages[0].key?('callid')).to be true
+
+    callid = res.messages[0]['callid']
+    res = @xc.get_simple_playback_status(callid: callid)
+    expect(res.errors).to be nil
+    expect(res.message.key?('callid')).to be true
+    expect(res.messages).to eq []
+  end
+
+  it 'test get simple playback status, fail' do
+    res = @xc.get_simple_playback_status(callid: "dabd8e76-390f-421c-87b5-57f31339d0c5")
+    expect(res.errors).to be 404
+    expect(res.message).to eq({})
     expect(res.messages).to eq []
   end
 end
+
